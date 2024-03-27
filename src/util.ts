b3df31712b8dcd06d3248436a51269b981422477
miigotu <miigotu@gmail.com>
2024-03-27 03:14:22 -0400

    Update util.ts

--- src/util.ts
+++ src/util.ts
@@ -9,6 +9,6 @@ export const IS_LINUX = process.platform === "linux";
 export const IS_MAC = process.platform === "darwin";
 
 // https://github.com/actions/toolkit/blob/819157bf872a49cfcc085190da73894e7091c83c/packages/core/src/core.ts#L126
-export function setInput(name: string, value: string) {
+export function setInput(name: string, value: any) {
   process.env[`INPUT_${name.replace(/[- ]/g, "_").toUpperCase()}`] = value;
 }
